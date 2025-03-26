75533fb26445de76705cd062f6f0467ad242ec9a
PavelPomykalov <102617293+PavelPomykalov@users.noreply.github.com>
2025-03-26 10:37:36 +0500

    Add Models for GetBookingId and test

---
+++ src/main/java/core/models/BookingDates.java
@@ -0,0 +1,37 @@
+package core.models;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class BookingDates {
+    private String checkin;
+    private String checkout;
+
+    // Конструктор
+    @JsonCreator
+
+    public BookingDates (
+            @JsonProperty("checkout") String checkout,
+            @JsonProperty ("checkin") String checkin) {
+        this.checkout = checkout;
+        this.checkin = checkin;
+    }
+
+    // геттеры и сеттеры
+
+    public String getCheckin() {
+        return checkin;
+    }
+
+    public void setCheckin(String checkin) {
+        this.checkin = checkin;
+    }
+
+    public String getCheckout() {
+        return checkout;
+    }
+
+    public void setCheckout(String checkout) {
+        this.checkout = checkout;
+    }
+}
