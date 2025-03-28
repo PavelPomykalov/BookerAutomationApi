483c95409bbbf35f428dc0aa9d3a55ea397e7ca2
PavelPomykalov <102617293+PavelPomykalov@users.noreply.github.com>
2025-03-28 20:35:50 +0500

    Add CreateToken and DeleteId

--- src/main/java/core/clients/APIClient.java
+++ src/main/java/core/clients/APIClient.java
@@ -2,7 +2,11 @@ package core.clients;
 
 import core.settings.ApiEndpoints;
 import io.restassured.RestAssured;
+import io.restassured.filter.Filter;
+import io.restassured.filter.FilterContext;
 import io.restassured.response.Response;
+import io.restassured.specification.FilterableRequestSpecification;
+import io.restassured.specification.FilterableResponseSpecification;
 import io.restassured.specification.RequestSpecification;
 
 import java.io.IOException;
@@ -11,6 +15,7 @@ import java.util.Properties;
 
 public class APIClient {
     private final String baseUrl;
+    private String token;
 
     public APIClient() {
         this.baseUrl = determineBaseUrl();
@@ -37,7 +42,36 @@ public class APIClient {
         return RestAssured.given()
                 .baseUri(baseUrl)
                 .header("Content-Type", "application/json")
-                .header("Accept", "application/json");
+                .header("Accept", "application/json")
+                .filter(addAuthTokenFilter());
+    }
+
+    //Метод для получения токена
+    public void createToken(String username, String password){
+        // Тело запрос для получения токена
+        String requestBody = String.format("{\"username\": \"%s\", \"password\": \"%s\" }", username, password);
+
+        Response response =  getRequestSpec()
+                .body(requestBody)
+                .when()
+                .post(ApiEndpoints.AUTH.getPath()) //Используем ENUM для эндпоинта /auth
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        // Извлекаем токен из ответа
+        token = response.jsonPath().getString("token");
+    }
+
+    // Фильтр для добавления токена в заголовок Authorization
+    private Filter addAuthTokenFilter(){
+        return (FilterableRequestSpecification requestSpec, FilterableResponseSpecification responseSpec, FilterContext ctx)-> {
+            if (token != null) {
+                requestSpec.header("Cookie", "token = " + token);
+            }
+            return ctx.next(requestSpec, responseSpec); // Продолжает выполнение запроса
+        };
     }
 
     // Get запрос на эндпоинт /ping
@@ -63,16 +97,29 @@ public class APIClient {
     }
 
     // Get запрос на эндпоинт /booking /{id}
-    public Response getBookingId(int id) {
-        return getRequestSpec() // это то что мы поместили в getRequestSpec
+    public Response getBookingId(int bookingid) {
+        return getRequestSpec()// это то что мы поместили в getRequestSpec
                 .when() // команда объявляет то что мы будем делать
-                .get(ApiEndpoints.BOOKING.getPath() + "/" + id) //отправляем get запрос //  Используем Enum для эндроинта /ping
+                .get(ApiEndpoints.BOOKING.getPath() + "/" + bookingid) //отправляем get запрос //  Используем Enum для эндроинта /booking
                 .then() //Затем
-                .statusCode(200) // Ожидаемый статус код
                 .extract() // Распаковка того, что пришло в response
                 .response();
-
     }
+
+    // DELETE запрос на эндпоинт /booking
+    public Response deleteBooking (int bookingid) {
+        return getRequestSpec()// это то что мы поместили в getRequestSpec
+                .when() // команда объявляет то что мы будем делать
+                .delete(ApiEndpoints.BOOKING.getPath() + "/" + bookingid)  //  Используем Enum для эндроинта /booking
+                .then() //Затем
+                .log().all()
+                .statusCode(201)// Ожидаемый статус код
+                .extract() // Распаковка того, что пришло в response
+                .response();
+    }
+
+
+
 }
 
 
